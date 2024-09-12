4f5c597969810266e659044ad3b32fdc6490a019
Vincent <vincentmu3@hotmail.com>
2024-09-12 14:50:32 +0200

    klar

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-const withPWA = require('next-pwa')({
-    dest: 'public', // Anger var den cachade service workern ska lagras
-  });
-  
-  module.exports = withPWA({
-    // Andra Next.js-konfigurationer kan också placeras här om nödvändigt
-    reactStrictMode: true,
-  });
-  
+const config = withPWA({
+  dest: 'public',
+  reactStrictMode: true,
+});
 
-export default nextConfig;
+export default config;
