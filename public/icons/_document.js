ce822e1cb12ba38382ba40938eac8afdb8ddf186
Vincent <vincentmu3@hotmail.com>
2024-09-12 15:11:57 +0200

    bild

--- public/icons/_document.js
+++ public/icons/_document.js
@@ -1,19 +1,27 @@
-import Document, { Html,Head,Main,NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render(){
-        return (
-            <Html>
-                <Head>
-                    <link rel="manifest" href="/manifest.json" />
-                    <link rel="apple-touch-icon" href="/icon.png"></link>
-                    <meta name="theme-color" content="#000000" />
-                </Head>
-                <body>
-                    <Main/>
-                    <NextScript/>
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Manifest and theme color */}
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#000000" />
+
+          {/* Apple Touch Icon */}
+          <link rel="apple-touch-icon" href="/Icons/icon-192x192.png" />
+
+          {/* Favicon (optional) */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
